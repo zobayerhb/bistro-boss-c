d12fa711a7a4474f836b3b954bc9a5b49df5872b
zobayerhb <hosenzobayer55@gmail.com>
2025-02-28 22:22:03 +0600

    delete user by admin

--- src/pages/Dashboard/AllUser/AllUser.jsx
+++ src/pages/Dashboard/AllUser/AllUser.jsx
@@ -2,17 +2,62 @@ import { FaTrashAlt, FaUsers } from "react-icons/fa";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import { useQuery } from "@tanstack/react-query";
 import useSecureAxios from "../../../hooks/useSecureAxios";
+import toast from "react-hot-toast";
 
 const AllUser = () => {
   const axiosSecure = useSecureAxios();
 
-  const { data: users = [] } = useQuery({
+  const { data: users = [], refetch } = useQuery({
     queryKey: ["users"],
     queryFn: async () => {
       const { data } = await axiosSecure.get("/users");
       return data;
     },
   });
+
+  const handleDelete = (id) => {
+    axiosSecure
+      .delete(`/users/${id}`)
+      .then((result) => {
+        const user = result.data;
+        if (user.deletedCount > 0) {
+          toast.success("User Succesfully Delete");
+        }
+        refetch();
+      })
+      .catch((error) => toast.error(error.message));
+  };
+
+  const handleCofirmDelete = (id) => {
+    toast((t) => (
+      <div className="flex items-center">
+        <div>
+          <p>
+            Are You <b>Sure?</b>
+          </p>
+        </div>
+        <div className="space-x-1">
+          <button
+            onClick={() => {
+              toast.dismiss(t.id);
+              handleDelete(id);
+            }}
+            className="text-white px-4 py-0.5 rounded bg-red-500 text-sm"
+          >
+            Yes
+          </button>
+          <button
+            className="px-4 py-0.5 text-sm bg-green-400 text-white rounded"
+            onClick={() => {
+              toast.dismiss(t.id);
+            }}
+          >
+            No
+          </button>
+        </div>
+      </div>
+    ));
+  };
 
   return (
     <div>
@@ -46,7 +91,7 @@ const AllUser = () => {
                   </td>
                   <th>
                     <button
-                      onClick={() => {}}
+                      onClick={() => handleCofirmDelete(user._id)}
                       className="btn btn-error btn-md text-white"
                     >
                       <FaTrashAlt />
