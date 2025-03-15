71a459d19947b26b872f828c8c08cd16e104a841
zobayerhb <hosenzobayer55@gmail.com>
2025-03-15 23:18:09 +0600

    send price data on the backend

--- src/pages/Dashboard/Payment/CheckOutForm.jsx
+++ src/pages/Dashboard/Payment/CheckOutForm.jsx
@@ -1,10 +1,32 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import useSecureAxios from "../../../hooks/useSecureAxios";
+import useCart from "../../../hooks/useCart";
 
 const CheckOutForm = () => {
+  const [clientSecret, setClientSecret] = useState("");
   const [error, setError] = useState("");
   const stripe = useStripe();
   const elements = useElements();
+  const axiosSecure = useSecureAxios();
+  const [cart] = useCart();
+  const totalPrice = cart.reduce(
+    (total, item) => total + Number(item.price || 0),
+    0
+  );
+  console.log("calculate total price", totalPrice);
+
+  useEffect(() => {
+    if (totalPrice > 0) {
+      console.log("total price ---> ", totalPrice);
+      axiosSecure
+        .post("/create-payment-intent", { price: totalPrice })
+        .then((res) => {
+          console.log(res.data.clientSecret);
+          setClientSecret(res.data.clientSecret);
+        });
+    }
+  }, [axiosSecure, totalPrice]);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -49,7 +71,7 @@ const CheckOutForm = () => {
       <button
         className="btn btn-md btn-primary my-5"
         type="submit"
-        disabled={!stripe}
+        disabled={!stripe || !clientSecret}
       >
         Pay
       </button>
